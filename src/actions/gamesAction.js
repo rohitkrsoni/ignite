4eddfc67aa9a0839638a077be4d978712f2c5e43
rohitkrsoni <krrohit37@gmail.com>
2023-05-06 17:22:08 +0530

    Fetched upcoming and newGames

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamsesURL, newGamesURL } from "../api";
 
 // Action Creator
 
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesURL());
+  const newGamesDate = await axios.get(newGamesURL());
+  const upcomingData = await axios.get(upcomingGamsesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesDate.data.results,
     },
   });
 };
