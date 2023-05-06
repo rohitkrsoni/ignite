fd9d6e5b621f8a502408d8f929b568017576c299
rohitkrsoni <krrohit37@gmail.com>
2023-05-06 19:25:11 +0530

    Add action reducer for game detail

--- src/api.js
+++ src/api.js
@@ -32,3 +32,10 @@ const new_games = `games?${key}&dates=${lastYear},${currentDate}&ordering=-relea
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamsesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
+
+// GAME DETAILS
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}?${key}`;
+
+// GAME DETAILS
+export const gameScreenShotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?${key}`;
