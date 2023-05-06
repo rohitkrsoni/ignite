efd89fd689342d6f4d5b8e8fdf4dba74da9c81f4
rohitkrsoni <krrohit37@gmail.com>
2023-05-06 14:33:41 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Iginite</h1>
+    </div>
+  );
+}
+
+export default App;
+//  Install packeges npm install redux react-redux react-router-dom styled-components framer-motion redux-thunk
